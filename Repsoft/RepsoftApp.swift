f72264520e1dcf3861c3e9a81be1d150e3b84b83
ChetaNandwal <chetannandwal05@gmail.com>
2023-01-09 16:29:13 +0530

    Initial Commit

---
+++ Repsoft/RepsoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepsoftApp.swift
+//  Repsoft
+//
+//  Created by chetan nandwal on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RepsoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
